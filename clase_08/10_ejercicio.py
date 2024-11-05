f3623a4a4b8efea2b283a8f303b4880cae8facc0
Patricio Morón <patriciomoron@gmail.com>
2024-11-05 10:56:55 -0300

    ejercicio clase 8, falta arreglar que no me repita el ultimo dato de productos

--- clase_08/10_ejercicio.py
+++ clase_08/10_ejercicio.py
@@ -11,3 +11,35 @@ datos = {
 }
 print(f"El producto {datos['producto']} cuesta $ {datos['precio']} y su stock es {datos['cantidad']}")
 """
+def ingresar_datos_producto():
+    nombre_producto = input("Ingrese el nombre del producto: ")
+    precio_producto = float(input("Ingrese el precio del producto: "))
+    stock_producto = int(input("Ingrese la cantidad en stock del producto: "))
+
+    datos_producto["nombre_producto"] = nombre_producto
+    datos_producto["precio_producto"] = precio_producto
+    datos_producto["stock_producto"] = stock_producto
+    return datos_producto
+
+def mostrar_datos_producto(producto):
+
+    print("Carrito de compras:")
+    print()
+    for i in range(len(producto)):
+        print(f"Producto {i}: {producto[i]['nombre_producto']}, Precio: ${producto[i]['precio_producto']}, Cantidad: {producto[i]['stock_producto']}")
+    print()
+
+
+datos_producto = {}
+carrito_compras = []
+def main():
+    for i in range(3):
+        print("---------------------------------")
+        print(f"INGRESE LOS DATOS DEL PRODUCTO {i+1}:")
+        print("---------------------------------")
+        datos_producto = ingresar_datos_producto()
+        carrito_compras.append(datos_producto)
+    print()
+    mostrar_datos_producto(carrito_compras)
+
+main()
